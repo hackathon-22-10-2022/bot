c29836d7a8d930df5883ebfc3ecc59b7289ae97d
Kirill Kulikov <to+git@adoge.ru>
2022-10-22 20:02:25 +0300

    feat(hello-world): remove dialogs

--- handlers/start.py
+++ handlers/start.py
@@ -1,8 +1,6 @@
-from forms.states import MySG
 from aiogram.types import Message
-from aiogram_dialog import DialogManager, StartMode
 
 
-async def start(m: Message, dialog_manager: DialogManager):
-    await dialog_manager.start(MySG.main, mode=StartMode.RESET_STACK)
+async def start(message: Message):
+    await message.answer('Hello, World!')
 
--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import logging
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-from aiogram_dialog import Window, Dialog, DialogRegistry
-from aiogram_dialog.widgets.kbd import Button
-from aiogram_dialog.widgets.text import Const
 from handlers import start
 from config import config
-from forms.states import MySG
 
 storage = MemoryStorage()
 bot = Bot(token=config.TELEGRAM_TOKEN_BOT)
 dp = Dispatcher(bot, storage=storage)
-registry = DialogRegistry(dp)
+
 
 logging.basicConfig(
 	level=logging.INFO,
@@ -22,18 +18,10 @@ logging.basicConfig(
 )
 
 
-main_window = Window(
-    Const("Hello, unknown person"),
-    Button(Const("Useless button"), id="nothing"),
-    state=MySG.main,
-)
-
-dialog = Dialog(main_window)
-registry.register(dialog)
-
 dp.register_message_handler(start.start, commands=['start'])
 
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+
