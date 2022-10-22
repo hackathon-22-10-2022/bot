dbb301a18aa7c56bfb5071527e2728c05d45815d
Kirill Kulikov <to+git@adoge.ru>
2022-10-22 16:53:36 +0300

    fix(main): async

--- main.py
+++ main.py
@@ -5,16 +5,11 @@ from config import config, mongo_config
 
 	
 async def do_insert():
-    document = {'name': '1234567890'}
+    document = {'name2': '1234567890222'}
     result = await mongo_config.get_mongo_collection().insert_one(document)
     print('result %s' % repr(result.inserted_id))
 
 
-loop = mongo_config.get_mongo_client().get_io_loop()
-print(mongo_config.get_mongo_client())
-loop.run_until_complete(do_insert())
-
-
-
+asyncio.run(do_insert())
 
 
