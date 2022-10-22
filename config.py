be85739045b206f0fd72e6e7c7cf9967ad388f11
Kirill Kulikov <to+git@adoge.ru>
2022-10-22 16:47:29 +0300

    feat(mongo): handle

--- config.py
+++ config.py
@@ -1,16 +1,29 @@
-import imp
 import os
 from urllib.parse import quote_plus
 
-import pymongo
+from motor import motor_asyncio
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config:
-	TELEGRAM_TOKEN_BOT = os.getenv('TELEGRAM_TOKEN_BOT')
-	MONGO_DB_URL = os.getenv('MONGO_DB_URL')
+    def __init__(self):
+        self.TELEGRAM_TOKEN_BOT = os.getenv('TELEGRAM_TOKEN_BOT')
+        self.MONGO_DB_URL = os.getenv('MONGO_DB_URL')
 
-	def get_mongo_client(self):
-		return pymongo.MongoClient(self.MONGO_DB_URL)
+
+class MongoConfig:
+    @staticmethod
+    def get_mongo_client() -> motor_asyncio.AsyncIOMotorClient:
+        return motor_asyncio.AsyncIOMotorClient(Config().MONGO_DB_URL)
+    
+    def get_mongo_collection(self) -> motor_asyncio.AsyncIOMotorCollection:
+        return self.get_mongo_client().hackathon_22_10_2022.spetsdor
 
 
 config = Config()
+mongo_config = MongoConfig()
+
+
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from config import config
+import asyncio
 
-
-
-def hello():
-	print(config.get_mongo_client())
+from config import config, mongo_config
 
 
 	
+async def do_insert():
+    document = {'name': '1234567890'}
+    result = await mongo_config.get_mongo_collection().insert_one(document)
+    print('result %s' % repr(result.inserted_id))
 
 
-if __name__ == "__main__":
-	hello()
+loop = mongo_config.get_mongo_client().get_io_loop()
+print(mongo_config.get_mongo_client())
+loop.run_until_complete(do_insert())
+
+
+
+
 
